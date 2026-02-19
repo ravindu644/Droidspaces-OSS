a4d8d8636e87f6f7704a868dedd1c10ed791bbb2
ravindu644 <droidcasts@protonmail.com>
2026-02-19 19:58:22 +0530

    bump version to v3.3.0

--- src/droidspace.h
+++ src/droidspace.h
@@ -51,7 +51,7 @@
  * ---------------------------------------------------------------------------*/
 
 #define DS_PROJECT_NAME "Droidspaces"
-#define DS_VERSION "3.2.2"
+#define DS_VERSION "3.3.0"
 #define DS_AUTHOR "ravindu644, Antigravity"
 #define DS_REPO "https://github.com/ravindu644/Droidspaces-OSS"
 #define DS_MAX_TTYS 6
