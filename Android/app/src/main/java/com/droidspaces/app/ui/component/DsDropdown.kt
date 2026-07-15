703f8792ec1c3de5ff8f9227595aa66f68c1fdf6
VizXtreme <sk06pandit@gmail.com>
2026-07-15 03:33:20 +0000

    fix: Add missing Color import in DsDropdown

--- Android/app/src/main/java/com/droidspaces/app/ui/component/DsDropdown.kt
+++ Android/app/src/main/java/com/droidspaces/app/ui/component/DsDropdown.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
